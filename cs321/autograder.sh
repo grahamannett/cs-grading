1526df01b566034bf372b02ca9a8b146cb4da12f
graham <graham.annett@gmail.com>
2023-09-11 15:29:57 -0600

    need to clone before tests for files

--- cs321/autograder.sh
+++ cs321/autograder.sh
@@ -4,7 +4,7 @@ PROJECT_NAME="${1:-$PROJECT_NAME}"
 BASE_REPO_PATH="../cs321-resources"
 RUBRIC_FILE=*-rubric.txt
 
-# exit 0 so no errors 
+# exit 0 so no errors
 ! test -n "$PROJECT_NAME" && echo "PROJECT_NAME must be set" && exit 0
 
 check_for() {
@@ -32,12 +32,11 @@ run_tests() {
 #         ;;
 #     esac
 # }
+! test -d $BASE_REPO_PATH && git clone https://github.com/BoiseState/CS321-resources $BASE_REPO_PATH
 
 check_for test-cases
 check_for run-tests.sh && chmod +x run-tests.sh
 check_for $RUBRIC_FILE
 
-! test -d $BASE_REPO_PATH && git clone https://github.com/BoiseState/CS321-resources $BASE_REPO_PATH
-
 # might need `|| true`
-run_tests 
+run_tests
